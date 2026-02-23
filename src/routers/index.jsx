7e203e0cb0fb9ab43600ba9ea9466a1e1b2fee88
Natsuda006 <664259006@webmail.npru.ac.th>
2026-02-23 11:07:39 +0700

    add loginpage

--- src/routers/index.jsx
+++ src/routers/index.jsx
@@ -2,18 +2,23 @@ import { createBrowserRouter } from "react-router";
 
 import MainLayout from "../layouts/mainLayout";
 import Home from "../pages/home";
+import Login from "../pages/login";
 
 const router = createBrowserRouter([
-    {
+  {
+    path: "/",
+    element: <MainLayout />,
+    children: [
+      {
         path: "/",
-        element: <MainLayout />,
-        children: [
-            {
-                path: "/",
-                element: <Home />,
-            },
-        ],
-    },
+        element: <Home />,
+      },
+      {
+        path: "/login",
+        element: <Login />,
+      },
+    ],
+  },
 ]);
 
 export default router;
